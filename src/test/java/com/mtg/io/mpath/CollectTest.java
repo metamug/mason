f2bed36c42d20c0c40a8e284a2cc571813e01060
Kaustubh Damle <kd.damle@gmail.com>
2018-01-20 13:33:12 +0530

    Updated input data string

--- src/test/java/com/mtg/io/mpath/CollectTest.java
+++ src/test/java/com/mtg/io/mpath/CollectTest.java
@@ -15,25 +15,32 @@ import org.junit.Test;
  * @author anishhirlekar
  */
 public class CollectTest {
-    public static final String inputJson1 = "[\n" +
-"       {\"name\":\"Anish\", \"books\":{\"name\":\"book2\"}}, \n" +
-"       {\"name\":\"Anish\", \"books\":{\"name\":\"book4\"}}, \n" +
-"       {\"name\":\"Anish\", \"books\":{\"name\":\"book5\"}}, \n" +
-"       {\"name\":\"Anish\", \"books\":{\"name\":\"book6\"}}\n" +
-"    ]";
-    
-    @Test
-    public void TestWhetherNotJsonArray(){
+
+    private static final String INPUT_JSON = "[\n"
+            + "       {\"name\":\"Anish\", \"books\":{\"name\":\"book1\",\"price\":\"10\"},\"number\":\"8080\"}, \n"
+            + "       {\"name\":\"Anish\", \"books\":{\"name\":\"book2\",\"price\":\"11\"},\"number\":\"8080\"}, \n"
+            + "       {\"name\":\"Anish\", \"books\":{\"name\":\"book3\",\"price\":\"12\"},\"number\":\"8080\"}, \n"
+            + "       {\"name\":\"Anish\", \"books\":{\"name\":\"book4\",\"price\":\"13\"},\"number\":\"8080\"}, \n"
+            + "       {\"name\":\"Anish\", \"books\":{\"name\":\"book5\",\"price\":\"14\"},\"number\":\"8080\"}, \n"
+            + "       {\"name\":\"Anish\", \"books\":{\"name\":\"book6\",\"price\":\"15\"},\"number\":\"8080\"}, \n"
+            + "       {\"name\":\"Anish\", \"books\":{\"name\":\"book7\",\"price\":\"16\"},\"number\":\"8080\"}, \n"
+            + "       {\"name\":\"Anish\", \"books\":{\"name\":\"book8\",\"price\":\"17\"},\"number\":\"8080\"}, \n"
+            + "       {\"name\":\"Anish\", \"books\":{\"name\":\"book9\",\"price\":\"18\"},\"number\":\"8080\"}, \n"
+            + "       {\"name\":\"Anish\", \"books\":{\"name\":\"book10\",\"price\":\"19\"},\"number\":\"8080\"}\n"
+            + "    ]";
+
+    // @Test
+    public void TestWhetherNotJsonArray() {
         JSONObject obj = new JSONObject();
         obj.put("name", "Anish");
         obj.put("age", "22");
-                
+
         Object name = obj.get("name");
         Assert.assertFalse(name instanceof JSONArray);
     }
-    
-    @Test
-    public void TestWhetherJsonArray(){
+
+    //@Test
+    public void TestWhetherJsonArray() {
         JSONObject obj = new JSONObject();
         JSONArray names = new JSONArray();
         names.put("Anish");
@@ -41,14 +48,14 @@ public class CollectTest {
         names.put("Kaustubh");
         obj.put("name", names);
         obj.put("age", "22");
-                
+
         Object name = obj.get("name");
         Assert.assertTrue(name instanceof JSONArray);
     }
-    
+
     @Test
-    public void TestCollect(){
-        JSONObject object = MPathUtil.collect(new JSONArray(inputJson1));
+    public void TestCollect() {
+        JSONObject object = MPathUtil.collect(new JSONArray(INPUT_JSON));
         System.out.println(object);
     }
 }
