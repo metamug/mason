8c26237d6d6fd813b8ef7f324d467cc004aec957
Kaustubh Damle <kaustubh.damle@metamug.com>
2018-08-14 17:17:06 +0530

    Corrected column name 'request_method' in place of 'method' while logging errors

--- src/main/java/com/metamug/api/taghandlers/ExceptionTagHandler.java
+++ src/main/java/com/metamug/api/taghandlers/ExceptionTagHandler.java
@@ -476,7 +476,7 @@ public class ExceptionTagHandler extends BodyTagSupport implements TryCatchFinal
             exceptionMessage = exception.toString();
         }
         try (Connection con = ds.getConnection()) {
-            PreparedStatement stmnt = con.prepareStatement("INSERT INTO error_log (error_id,method,message,resource)"
+            PreparedStatement stmnt = con.prepareStatement("INSERT INTO error_log (error_id,request_method,message,resource)"
                     + " VALUES(?,?,?,?)");
             stmnt.setString(1, String.valueOf(errorId));
             stmnt.setString(2, method);
