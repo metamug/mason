7bd338bbf056e410653ccf2e12b8ba51cc7a47af
Anish Hirlekar <anish.hirlekar@metamug.com>
2019-01-22 00:23:57 +0530

    Refactored test package names

--- src/test/java/com/metamug/mason/entity/auth/JWebTokenTest.java
+++ src/test/java/com/metamug/mason/entity/auth/JWebTokenTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.metamug.api.common;
+package com.metamug.mason.entity.auth;
 
 import com.metamug.mason.entity.auth.JWebToken;
 import java.security.NoSuchAlgorithmException;
--- src/test/java/com/metamug/mason/entity/response/MasonOutputTest.java
+++ src/test/java/com/metamug/mason/entity/response/MasonOutputTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.metamug.mason.entity;
+package com.metamug.mason.entity.response;
 
 import com.metamug.mason.entity.response.XMLOutput;
 import com.metamug.mason.entity.response.MasonOutput;
