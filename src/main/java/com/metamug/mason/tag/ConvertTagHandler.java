6b81ca86e6d9ec1aaea3fd62b7cb35c5b517ee0f
Anish Hirlekar <anish.hirlekar@metamug.com>
2019-02-09 11:45:35 +0530

    Added convert tag

---
+++ src/main/java/com/metamug/mason/tag/ConvertTagHandler.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.metamug.mason.tag;
+
+import java.util.LinkedHashMap;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+import javax.servlet.jsp.tagext.TryCatchFinally;
+import org.apache.taglibs.standard.tag.common.sql.ResultImpl;
+
+/**
+ *
+ * @author anishhirlekar
+ */
+public class ConvertTagHandler extends BodyTagSupport implements TryCatchFinally {
+    private Object result;
+    private Object target;
+    private String property;
+    
+    @Override
+    public int doEndTag() throws JspException {
+        LinkedHashMap<String, Object> targetMap = (LinkedHashMap<String, Object>) target;
+        ResultImpl resultSet = (ResultImpl)result;
+        //todo convert resultimpl to map
+        
+        return EVAL_PAGE;
+    }
+    
+    public void setResult(Object r){
+        result = r;
+    }
+    
+    public void setTarget(Object t){
+        target = t;
+    }
+    
+    public void setProperty(String p){
+        property = p;
+    }
+
+    @Override
+    public void doCatch(Throwable thrwbl) throws Throwable {
+        throw thrwbl;
+    }
+
+    @Override
+    public void doFinally() {
+    }
+}
