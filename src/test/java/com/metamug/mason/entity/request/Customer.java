aece0b9a125fe56358d52bd5302241f9ec5d94f2
richard937 <papuncharan@gmail.com>
2020-05-19 23:49:52 +0530

    Added test for JsonBodyStrategy

---
+++ src/test/java/com/metamug/mason/entity/request/Customer.java
@@ -0,0 +1,35 @@
+package com.metamug.mason.entity.request;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Customer {
+    private int id;
+    private int roll;
+    private String name;
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
--- src/test/java/com/metamug/mason/entity/request/JsonBodyStrategyTest.java
+++ src/test/java/com/metamug/mason/entity/request/JsonBodyStrategyTest.java
@@ -6,11 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.io.*;
+
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class JsonBodyStrategyTest {
@@ -19,20 +18,39 @@ public class JsonBodyStrategyTest {
     private HttpServletRequest request;
 
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
         request = mock(HttpServletRequest.class);
-        ServletContext context = mock(ServletContext.class);
-        when(request.getServletContext()).thenReturn(context);
-        when(context.getContextPath()).thenReturn("backend");
+        String json = "{\n" +
+                "    \"customer\": {\n" +
+                "      \"contact\": {\n" +
+                "        \"phone\": \"+1 943 322 4292\",\n" +
+                "        \"email\": \"john.doe@gmail.com\"\n" +
+                "      },\n" +
+                "      \"name\": \"John Doeyy.\",\n" +
+                "      \"roll\": 555,\n" +
+                "      \"id\": 8\n" +
+                "    }\n" +
+                "  }";
+
+        Reader inputString = new StringReader(json);
+        BufferedReader reader = new BufferedReader(inputString);
+
+        when(request.getReader()).thenReturn(reader);
+
     }
 
     @Test
     public void jsonBodyUnmarshal() throws IOException {
 
             JsonBodyStrategy masonRequest = new JsonBodyStrategy(request);
+            masonRequest.setClazz(Customer.class);
             Object object = masonRequest.getBodyObject();
 
-            Assert.assertNotNull(object);
+            Customer customer = (Customer) object;
+
+            String name = customer.getName();
+            System.out.println(customer);
+            Assert.assertEquals("John Doeyy.", name);
     }
 
 }
