7d3d43a5c07f64e0ae2e0fc3e9800dc234e54786
richard937 <papuncharan@gmail.com>
2020-06-10 16:06:53 +0530

    422 error

--- src/main/java/com/metamug/mason/tag/RequestTagHandler.java
+++ src/main/java/com/metamug/mason/tag/RequestTagHandler.java
@@ -589,7 +589,7 @@ public class RequestTagHandler extends RequestTag {
             throw new JspException("Unable to find the specified class");
         } catch (IOException | JAXBException ex) {
             Logger.getLogger(RequestTagHandler.class.getName()).log(Level.SEVERE, null, ex);
-            throw new JspException("Status: 200 \n Could not parse the body of the request according to the provided Content-Type.");
+            throw new JspException("Status: 422 \n Could not parse the body of the request according to the provided Content-Type.");
         }
 
     }
