c66de04d5ca7cfb11bb46acd1a468499ba71f439
Deepak Mishra <d3ep4k@gmail.com>
2020-06-12 18:27:08 +0530

    updated server port to 7000

--- server/src/main/java/launch/Main.java
+++ server/src/main/java/launch/Main.java
@@ -23,7 +23,7 @@ public class Main {
         //Look for that variable and default to 8080 if it isn't there.
         String webPort = System.getenv("PORT");
         if(webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
+            webPort = "7000";
         }
 
         tomcat.setPort(Integer.valueOf(webPort));
