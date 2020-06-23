3c2ccc48765a4e2797a5a4cbec0c3639e5eade98
richard937 <papuncharan@gmail.com>
2020-06-23 17:55:19 +0530

    created new branch

--- taglib/src/main/java/com/metamug/mason/entity/request/XmlBodyStrategy.java
+++ taglib/src/main/java/com/metamug/mason/entity/request/XmlBodyStrategy.java
@@ -1,13 +1,10 @@
 package com.metamug.mason.entity.request;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
